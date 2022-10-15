877c1d8abf58d676c0fd30f7779ba575ee9d1ad4
sabin <sabin0kharel@gmail.com>
2022-10-15 17:46:21 +0545

    form validation

---
+++ src/utils/validation/fileValidator.js
@@ -0,0 +1,11 @@
+export const ImageValidator = (file) => {
+  const validExtensions = ["png", "jpg", "jpeg", "webp"];
+  const fileExtension = file.type.split("/")[1];
+  return validExtensions.includes(fileExtension);
+};
+
+export const DocValidator = (file) => {
+  const validExtensions = ["doc", "docx", "odt", "pdf"];
+  const fileExtension = file.type.split("/")[1];
+  return validExtensions.includes(fileExtension);
+};
